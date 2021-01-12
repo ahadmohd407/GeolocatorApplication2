e770221a5684b23a20ab38d46aafa5bd13af8e53
mohd ahad <ahadmohd407>
2021-01-13 02:21:25 +0530

    Commited by Ahad

--- app/src/main/java/com/geolocatorapplication/MainActivity.java
+++ app/src/main/java/com/geolocatorapplication/MainActivity.java
@@ -1,14 +1,63 @@
 package com.geolocatorapplication;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.abhiandroid.fragmentexample.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton firstFragment, secondFragment,thirdFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+// get the reference of Button's
+        firstFragment = (ImageButton) findViewById(R.id.firstFragment);
+        secondFragment = (ImageButton) findViewById(R.id.secondFragment);
+        thirdFragment=(ImageButton)findViewById(R.id.thirdFragment);
+
+// perform setOnClickListener event on First Button
+        firstFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// load First Fragment
+                loadFragment(new FirstFragment());
+            }
+        });
+// perform setOnClickListener event on Second Button
+        secondFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// load Second Fragment
+                loadFragment(new SecondFragment());
+            }
+        });
+
+        thirdFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// load Second Fragment
+                loadFragment(new ThirdFragment());
+            }
+        });
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+// create a FragmentManager
+        FragmentManager fm = getFragmentManager();
+// create a FragmentTransaction to begin the transaction and replace the Fragment
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+// replace the FrameLayout with new Fragment
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit(); // save the changes
     }
 }
---
+++ app/src/main/java/com/geolocatorapplication/ThirdFragment.java
@@ -0,0 +1,48 @@
+package com.geolocatorapplication;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.abhiandroid.fragmentexample.R;
+
+//import com.abhiandroid.fragmentexample.R;
+
+public class ThirdFragment extends Fragment {
+
+
+    View view;
+    Button firstButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+// Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_third, container, false);
+// get the reference of Button
+        firstButton = (Button) view.findViewById(R.id.thirdButton);
+// perform setOnClickListener on first Button
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// display a message by using a Toast
+                Toast.makeText(getActivity(), "Third Fragment", Toast.LENGTH_LONG).show();
+            }
+        });
+        return view;
+    }
+}
+
+
+
+
+
+
+
+
+
+
