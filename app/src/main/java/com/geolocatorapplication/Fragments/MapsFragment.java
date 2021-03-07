20eb57b8f0ad67fc5799f831765580c5df0f9a4c
Adityaverma123 <av9567@gmail.com>
2021-03-08 00:10:57 +0530

    initial commit

--- app/src/main/java/com/geolocatorapplication/Fragments/MapsFragment.java
+++ app/src/main/java/com/geolocatorapplication/Fragments/MapsFragment.java
@@ -2,17 +2,21 @@ package com.geolocatorapplication.Fragments;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,58 +24,41 @@ import android.widget.Toast;
 
 import com.geolocatorapplication.MainActivity;
 import com.geolocatorapplication.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
-public class MapsFragment extends Fragment {
-    LocationManager locationManager;
-    LocationListener locationListener;
-    GoogleMap mMap;
+public class MapsFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener,com.google.android.gms.location.LocationListener {
+    private LocationManager locationManager;
+    private LocationManager mlocationManager;
+    private Location location;
+    private LocationRequest request;
+    private  long UPDATE_INTERVAL=2000;
+    private  long FASTEST_INTERVAL=5000;
+    private LatLng latLng;
+    private  boolean isPermission;
+    private  GoogleApiClient googleApiClient;
+    private LocationListener locationListener;
+    private  GoogleMap mMap;
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            mMap=googleMap;
-
-            locationManager=(LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
-            locationListener=new LocationListener() {
-                @Override
-                public void onLocationChanged(@NonNull Location location) {
-                    mMap.clear();
-                    LatLng userLoc = new LatLng(location.getLatitude(), location.getLongitude());
-                    mMap.addMarker(new MarkerOptions().position(userLoc).title("Your Location"));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(userLoc));
-                    mMap.animateCamera(CameraUpdateFactory.zoomBy(15));
-
-//                    mMap.animateCamera(CameraUpdateFactory.zoomBy(15), 2000, null);
-                }
-            };
-            if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
-            }
-            else{
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener );
-                Location lastLocation=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                mMap.clear();
-
-                try {
-                    LatLng userLoc = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
-                    mMap.addMarker(new MarkerOptions().position(userLoc).title("Your Location"));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLoc,15));
-                    mMap.animateCamera(CameraUpdateFactory.zoomIn());
-                    mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
-                }
-                catch (Exception e){
-                    e.printStackTrace();
-                }
-
-            }
-
-
+            mMap = googleMap;
 
         }
     };
@@ -103,10 +90,146 @@ public class MapsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        if(requestSinglePermission()) {
+            SupportMapFragment mapFragment =
+                    (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+            if (mapFragment != null) {
+                mapFragment.getMapAsync(callback);
+            }
+            googleApiClient=new GoogleApiClient.Builder(getContext())
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+            mlocationManager=(LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+            checkLocation();
+        }
+    }
+
+    private boolean checkLocation() {
+        if(!isLocationEnabled()){
+            AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+            builder.setTitle("Enable Location")
+                    .setMessage("Your Location settings is set to 'Off.'\n" +
+                            "Please enable location to use the app")
+                    .setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Intent intent=new Intent(Settings.ACTION_LOCALE_SETTINGS);
+                            startActivity(intent);
+
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+        }
+        return isLocationEnabled();
+    }
+
+    private boolean isLocationEnabled() {
+        locationManager=(LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    private boolean requestSinglePermission() {
+        Dexter.withActivity(getActivity())
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        isPermission=true;
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        isPermission=false;
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                });
+
+        return true;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if(ActivityCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        startLocationUpdate();
+        if(location==null){
+            startLocationUpdate();
+        }
+        else{
+            Toast.makeText(getActivity(),"Location not detected",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void startLocationUpdate() {
+        request=LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL)
+                .setFastestInterval(FASTEST_INTERVAL);
+        if(ActivityCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_COARSE_LOCATION)
+                !=PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient,
+                request,this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap=googleMap;
+        if(latLng!=null){
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Your Current Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,14));
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latLng=new LatLng(location.getLatitude(),location.getLongitude());
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(googleApiClient!=null){
+            googleApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(googleApiClient.isConnected()){
+                googleApiClient.disconnect();
         }
     }
 }
