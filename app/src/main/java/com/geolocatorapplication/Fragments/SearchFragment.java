73a98f799dc5c1af74d075b2f1a9d3bc8e78731b
Adityaverma123 <av9567@gmail.com>
2021-03-09 19:57:20 +0530

    added recent searches

--- app/src/main/java/com/geolocatorapplication/Fragments/SearchFragment.java
+++ app/src/main/java/com/geolocatorapplication/Fragments/SearchFragment.java
@@ -2,15 +2,27 @@ package com.geolocatorapplication.Fragments;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
 
+import com.geolocatorapplication.Adapters.RestaurantAdapter;
 import com.geolocatorapplication.R;
-public class SearchFragment extends Fragment {
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchFragment extends Fragment {
+    SearchView searchView;
+    List<String> names;
+    RecyclerView recyclerView;
+    RestaurantAdapter adapter;
     public SearchFragment() {
         // Required empty public constructor
     }
@@ -19,6 +31,35 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view=inflater.inflate(R.layout.fragment_search, container, false);
+        searchView=view.findViewById(R.id.searchView);
+        names=new ArrayList<>();
+        names.add("Restaurant-1");
+        names.add("Restaurant-2");
+        names.add("Restaurant-3");
+        names.add("Restaurant-4");
+        
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if(!names.contains(query)) {
+                    names.add(query);
+                    adapter.notifyDataSetChanged();
+                }
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        recyclerView=view.findViewById(R.id.recyclerView);
+        adapter=new RestaurantAdapter(getContext(),names);
+        GridLayoutManager manager=new GridLayoutManager(getContext(),2);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 }
